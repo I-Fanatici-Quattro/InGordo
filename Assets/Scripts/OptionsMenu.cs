ffc03365289961f2802769002edee17c250feb53
Alessio <a.like.p0@gmail.com>
2020-11-02 16:41:22 +0100

    Ho aggiunto il menu di gioco che ho creato fino ad adesso

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public Toggle fullscreenTog, vsyncTog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyGraphics()
+    {
+        //applico il fullscreen e chiedo per il vSync
+        Screen.fullScreen = fullscreenTog.isOn;
+
+        if(vsyncTog.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
