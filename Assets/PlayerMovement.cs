30fb093adbb1c26a2ae5ab71635b932db0877485
Alessandro Cancelli <alessandrocancelli1@gmail.com>
2020-11-06 09:41:22 +0100

    Modifiche Player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    private float screenWidth;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // animator.SetFloat("Speed", 0.2);
+        screenWidth = Screen.width;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            jump = true;
+        }
+        
+    }
+
+    //Move our character
+    void FixedUpdate()
+    {
+        controller.Move(0,false,jump);
+        jump = false;
+    }
+}
