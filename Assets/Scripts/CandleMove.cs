ec3da1e4667101fc68456db84d7c3b956e0fd71e
DESKTOP-NTM15JD\utente <olivierocaico@gmail.com>
2020-11-06 15:58:04 +0100

    Ho aggiunto script per la mappa e per l'arrivo del cibo

---
+++ Assets/Scripts/CandleMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleMove : MonoBehaviour
+{
+    public GameObject Spawn;
+    public GameObject Cac;
+    [SerializeField]
+    int ritardoPrimaApparizione =0;
+    float tempoDiApparizione=2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            InvokeRepeating ("a",ritardoPrimaApparizione,tempoDiApparizione);              
+    }
+
+    void Update()
+    { 
+
+    }
+
+
+    void a()
+    {
+        Spawn = Instantiate (Cac,transform.position,Quaternion.identity)as GameObject; 
+    }
+}
