c72eeae21820c36b45d7c182e9c5ca51b68517e2
Alessio <a.like.p0@gmail.com>
2020-11-07 23:26:27 +0100

    - Aggiunta opzione audio "Master", "Musica" e "SFX"
    - Aggiunta qualche effetto al menu
    
    - Completamento versione pre-alpha del menu e interfaccia utente

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,20 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Globalization;
 
 public class OptionsMenu : MonoBehaviour
 {
 
+    public AudioMixer theMixer;
+
+    public Slider mastSlider, musicSlider, sfxSlider;
+    public Text mastLabel, musicLabel, sfxLabel;
+
+
+    public AudioSource sfxLoop;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(PlayerPrefs.HasKey("MasterVol"))
+        {
+            theMixer.SetFloat("MasterVol", PlayerPrefs.GetFloat("MasterVol"));
+            mastSlider.value = PlayerPrefs.GetFloat("MasterVol");
+        }
+
+        if (PlayerPrefs.HasKey("MusicVol"))
+        {
+            theMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVol");
+        }
+
+        if (PlayerPrefs.HasKey("SFXVol"))
+        {
+            theMixer.SetFloat("SFXVol", PlayerPrefs.GetFloat("SFXVol"));
+            sfxSlider.value = PlayerPrefs.GetFloat("SFXVol");
+        }
+
+        mastLabel.text = (mastSlider.value + 80).ToString();
+        musicLabel.text = (musicSlider.value + 80).ToString();
+        sfxLabel.text = (sfxSlider.value + 80).ToString();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void SetMasterVol()
+    {
+        mastLabel.text = (mastSlider.value + 80).ToString();
+
+        theMixer.SetFloat("MasterVol", mastSlider.value);
+
+        PlayerPrefs.SetFloat("MasterVol", mastSlider.value);
+
+    }
+
+    public void SetMusicVol()
+    {
+        musicLabel.text = (musicSlider.value + 80).ToString();
+
+        theMixer.SetFloat("MusicVol", musicSlider.value);
+
+        PlayerPrefs.SetFloat("MusicVol", musicSlider.value);
+
+    }
+
+    public void SetSFXVol()
+    {
+        sfxLabel.text = (sfxSlider.value + 80).ToString();
+
+        theMixer.SetFloat("SFXVol", sfxSlider.value);
+
+        PlayerPrefs.SetFloat("MFXVol", sfxSlider.value);
+
+    }
+
+
+    public void PlaySFXLoop()
+    {
+        sfxLoop.Play();
+
+    }
+
+    public void StopSFXLoop()
+    {
+        sfxLoop.Stop();
     }
 
 }
