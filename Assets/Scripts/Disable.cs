70ed53ebccc4c70d261e1534f55a59cd9df59292
Alessio <a.like.p0@gmail.com>
2020-11-17 13:43:09 +0100

    Risolti grossi problemi legati a malfunzionamenti dopo aver eliminato le opzioni per regolare il menu

--- Assets/Scripts/Disable.cs
+++ Assets/Scripts/Disable.cs
@@ -7,21 +7,31 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class Disable : MonoBehaviour
 {
-
+    
     public Button mybutton;
     public Sprite audioongrande;
     public Sprite audiooffgrande;
     private int counter = 0;
+    private bool isMuted;
     // Start is called before the first frame update
     void Start()
     {
         mybutton = GetComponent<Button>();
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = isMuted;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+        PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
     }
 
     public void changeButton()
